fce464051d352bd52c537fea7afd1cb5c6230f99
Dániel Koszta <daniel.koszta@gmail.com>
2024-12-13 10:50:04 +0100

    Add instance IDs as output
    Change-Id: I8394a03db315e56ac33019ba43c14c7dd5a92238

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "my-ip-address" {
     description = "my-ip-address"
     value = aws_eip.my-public-ips[*].public_ip
 }
+
+output "my-instance-id" {
+    description = "my-instance-id"
+    value = aws_instance.my-servers[*].id
+}
